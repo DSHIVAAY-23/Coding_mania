4d513cb97a209eb85952c528507f531516a10e2b
dshivaay <raidivyank08@gmail.com>
2023-09-22 12:30:56 +0530

    Add

---
+++ rust_practice/rust_examples/closures/target/debug/deps/closures-fe31424bc0853373.d
@@ -0,0 +1,5 @@
+/data/Coding_mania/rust_practice/rust_examples/closures/target/debug/deps/closures-fe31424bc0853373.rmeta: src/main.rs
+
+/data/Coding_mania/rust_practice/rust_examples/closures/target/debug/deps/closures-fe31424bc0853373.d: src/main.rs
+
+src/main.rs:
---
+++ rust_practice/rust_examples/closures/target/debug/deps/closures-feee30d0d0fc043d.d
@@ -0,0 +1,5 @@
+/data/Coding_mania/rust_practice/rust_examples/closures/target/debug/deps/closures-feee30d0d0fc043d.rmeta: src/main.rs
+
+/data/Coding_mania/rust_practice/rust_examples/closures/target/debug/deps/closures-feee30d0d0fc043d.d: src/main.rs
+
+src/main.rs:
