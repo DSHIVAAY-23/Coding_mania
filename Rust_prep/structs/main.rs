ab40a146d9f1022b2ba7190ca94f76c169e30fd0
DSHIVAAY-23 <raidivyank08@gmail.com>
2025-01-06 20:39:20 +0530

    rust prep

--- Rust_prep/structs/main.rs
+++ Rust_prep/structs/main.rs
@@ -15,14 +15,30 @@ impl Shooters {
 struct Gun {
     name: String,
     gun_type: i32,
+    ammo:i32,
+    max_ammo:i32,
 }
 
 impl Gun {
     fn load(&self) {
         println!("{} is loading", self.name);
     }
-    fn fire(&self) {
-        println!("{} is firing", self.name);
+   fn fire(&mut self) {
+        if self.ammo > 0 {
+            println!("{} is firing", self.name);
+            self.ammo -= 1;
+        } else {
+            println!("{} is out of ammo! Reload needed.", self.name);
+        }
+        
+        println!("{} ammo count",self.ammo);
+    }
+
+    fn reload(&mut self) {
+        self.ammo = self.max_ammo;
+        println!("{} is reloaded to max ammo: {}", self.name, self.max_ammo);
+                println!("{} ammo count",self.ammo);
+
     }
 }
 
@@ -40,18 +56,22 @@ fn main() {
     };
 
     // Create a Gun instance
-    let pistol = Gun {
+    let mut pistol = Gun {
         name: String::from("Desert Eagle"),
         gun_type: 1,
+        ammo:18,
+        max_ammo:18,
     };
 
     // Call methods on Shooter
     shootout.run();
     shootout.jump();
+    
 
     // Call methods on Gun
     pistol.load();
     pistol.fire();
+    pistol.reload();
 
     // Using the Feature enum
     let gun_feature = Feature::Automatic;
